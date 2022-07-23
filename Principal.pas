7b31550593ce456b1ab5cd42253c25f9ccf7d91e
Willmer Gonzalez <Willmer2911@gmail.com>
2022-07-23 18:42:19 -0400

    Ini Proyecto

--- Principal.pas
+++ Principal.pas
@@ -3,11 +3,14 @@ unit Principal;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -19,6 +22,29 @@ var
 
 implementation
 
+uses
+  System.Win.ComObj;
+
 {$R *.dfm}
 
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  XLApp: OleVariant ;
+begin
+  XLApp := CreateOleObject('Excel.Application');
+  XLApp.Visible := True;
+
+  XLApp.workbooks.add;
+  XLApp.workbooks[1].worksheets[1].name := 'Cartera';
+  XLApp.workbooks[1].worksheets[1].cells(1,1) := 'Azul';
+  XLApp.workbooks[1].worksheets[1].cells(1,2) := 'Fondo';
+  XLApp.workbooks[1].worksheets[1].cells(1,3) := 'Color';
+  XLApp.workbooks[1].worksheets[1].cells(1,5) := 'Negrita';
+  XLApp.workbooks[1].worksheets[1].cells(2,1) := 'Combinar Celdas';
+
+//  XLApp.Quit;
+//  XLApp := Unassigned;
+end;
+
 end.
+
